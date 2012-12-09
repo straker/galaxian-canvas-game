0f5573df6809f88b31e22b8e82cbaa271e3e6979
Steven <stevenlambert503@gmail.com>
2012-12-08 19:06:03 -0700

    added part 2 files and assests

---
+++ part2/space_shooter_part_two.js
@@ -0,0 +1,461 @@
+/***************
+ * PART TWO - Create the player controlled ship and it's
+ * properties (move and shoot)
+ ***************/
+
+/* NOTES TO REMEMBER
+ * http://opengameart.org/content/spaceship-tutorial-0 (enemy space ship)
+ */
+ 
+/* RESOURCES
+ * 1. http://gameprogrammingpatterns.com/object-pool.html
+ * 2. http://www.canvasdemos.com/2010/12/15/5-performance-tips-for-your-html5-games/
+ * 3. http://www.html5rocks.com/en/tutorials/speed/quick/
+ * 4. http://www.kontain.com/fi/entries/94636/ (quote on performace)
+ * 5. http://dougmcinnes.com/html-5-asteroids/
+ */
+
+	
+/**
+ * Initialize the Game and start it.
+ */
+var avgFramerate = 0;
+var frameCount = 0;
+var elapsedCounter = 0;
+var lastFrame = Date.now();
+var thisFrame;
+var elapsed;
+var game = new Game();
+
+function init() {
+	if(game.init())
+		game.start();
+}
+
+
+/**
+ * Define an object to hold all our images for the game so images
+ * are only ever created once. This type of object is known as a 
+ * singleton.
+ */
+var imageRepository = new function() {
+	// Define images
+	this.empty = null;
+	this.background = new Image();
+	this.spaceship = new Image();
+	this.bullet = new Image();
+	
+	// Set images src
+	this.background.src = "imgs/bg.png";
+	this.spaceship.src = "imgs/ship.png";
+	this.bullet.src = "imgs/bullet.png";
+}
+
+
+/**
+ * Creates the Drawable object which will be the base class for
+ * all drawable objects in the game. Sets up defualt variables
+ * that all child objects will inherit, as well as the defualt
+ * functions. 
+ */
+function Drawable() {
+	this.init = function(x, y, width, height) {
+		// Defualt variables
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	this.speed = 0;
+	this.canvasWidth = 0;
+	this.canvasHeight = 0;
+	
+	// Define abstract function to be implemented in child objects
+	this.draw = function() {
+	};
+	this.move = function() {
+	};
+}
+
+
+/**
+ * Creates the Background object which will become a child of
+ * the Drawable object. The background is drawn on the "background"
+ * canvas and creates the illusion of moving by panning the image.
+ */
+function Background() {
+	this.speed = 1; // Redefine speed of the background for panning
+	
+	// Implement abstract function
+	this.draw = function() {
+		// Pan background
+		this.y += this.speed;
+		this.context.drawImage(imageRepository.background, this.x, this.y);
+		
+		// Draw another image at the top edge of the first image
+		this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight);
+
+		// If the image scrolled off the screen, reset
+		if (this.y >= this.canvasHeight)
+			this.y = 0;
+	};
+}
+// Set Background to inherit properties from Drawable
+Background.prototype = new Drawable();
+
+
+/**
+ * Creates the Bullet object which the ship fires. The bullets are
+ * drawn on the "main" canvas.
+ */
+function Bullet() {	
+	this.alive = false; // Is true if the bullet is currently in use
+	
+	/*
+	 * Sets the bullet values
+	 */
+	this.spawn = function(x, y, speed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.alive = true;
+	};
+
+	/*
+	 * Uses a "drity rectangle" to erase the bullet and moves it.
+	 * Returns true if the bullet moved of the screen, indicating that
+	 * the bullet is ready to be cleared by the pool, otherwise draws
+	 * the bullet.
+	 */
+	this.draw = function() {
+		this.context.clearRect(this.x, this.y, this.width, this.height);
+		this.y -= this.speed;
+		if (this.y <= 0 - this.height) {
+			return true;
+		}
+		else {
+			this.context.drawImage(imageRepository.bullet, this.x, this.y);
+		}
+	};
+	
+	/*
+	 * Resets the bullet values
+	 */
+	this.clear = function() {
+		this.x = 0;
+		this.y = 0;
+		this.speed = 0;
+		this.alive = false;
+	};
+}
+Bullet.prototype = new Drawable();
+
+
+/**
+ * Custom Pool object. Holds Bullet objects to be managed to prevent
+ * garbage collection. 
+ * The pool works as follows:
+ * - When the pool is initialized, it popoulates an array with 
+ *   Bullet objects.
+ * - When the pool needs to create a new object for use, it looks at
+ *   the last item in the array and checks to see if it is currently
+ *   in use or not. If it is in use, the pool is full. If it is 
+ *   not in use, the pool "spawns" the last item in the array and 
+ *   then pops it from the end and pushed it back onto the front of
+ *   the array. This makes the pool have free objects on the back 
+ *   and used objects in the front.
+ * - When the pool animates its objects, it checks to see if the 
+ *   object is in use (no need to draw unused objects) and if it is, 
+ *   draws it. If the draw() function returns true, the object is 
+ *   ready to be cleaned so it "clears" the object and uses the 
+ *   array function splice() to remove the item from the array and 
+ *   pushes it to the back.
+ * Doing this makes creating/destroying objects in the pool 
+ * constant.
+ */
+function Pool(maxSize) {
+	var size = maxSize; // Max bullets allowed in the pool
+	var pool = [];
+	
+	/*
+	 * Populates the pool array with Bullet objects
+	 */
+	this.init = function() {
+		for (var i = 0; i < size; i++) {
+			// Initalize the bullet object
+			var bullet = new Bullet();
+			bullet.init(0,0, imageRepository.bullet.width,
+			            imageRepository.bullet.height);
+			pool[i] = bullet;
+		}
+	};
+	
+	/*
+	 * Grabs the last item in the list and initializes it and
+	 * pushes it to the front of the array.
+	 */
+	this.get = function(x, y, speed) {
+		if(!pool[size - 1].alive) {
+			pool[size - 1].spawn(x, y, speed);
+			pool.unshift(pool.pop());
+		}
+	};
+	
+	/*
+	 * Used for the ship to be able to get two bullets at once. If
+	 * only the get() function is used twice, the ship is able to
+	 * fire and only have 1 bullet spawn instead of 2.
+	 */
+	this.getTwo = function(x1, y1, speed1, x2, y2, speed2) {
+		if(!pool[size - 1].alive && 
+		   !pool[size - 2].alive) {
+				this.get(x1, y1, speed1);
+				this.get(x2, y2, speed2);
+			 }
+	};
+	
+	/*
+	 * Draws any in use Bullets. If a bullet goes off the screen,
+	 * clears it and pushes it to the front of the array.
+	 */
+	this.animate = function() {
+		for (var i = 0; i < size; i++) {
+			// Only draw until we find a bullet that is not alive
+			if (pool[i].alive) {
+				if (pool[i].draw()) {
+					pool[i].clear();
+					pool.push((pool.splice(i,1))[0]);
+				}
+			}
+			else
+				break;
+		}
+	};
+}
+
+
+/**
+ * Create the Ship object that the player controls. The ship is
+ * drawn on the "ship" canvas and uses dirty rectangles to move
+ * around the screen.
+ */
+function Ship() {
+	this.speed = 3;
+	this.bulletPool = new Pool(30);
+	this.bulletPool.init();
+
+	var fireRate = 15;
+	var counter = 0;
+	this.collidableWith = "enemyBullet";
+	this.type = "ship";
+	
+	this.draw = function() {
+		this.context.drawImage(imageRepository.spaceship, this.x, this.y);
+	};
+	this.move = function() {	
+		counter++;
+		// Determine if the action is move action
+		if (KEY_STATUS.left || KEY_STATUS.right ||
+				KEY_STATUS.down || KEY_STATUS.up) {
+			// The ship moved, so erase it's current image so it can
+			// be redrawn in it's new location
+			this.context.clearRect(this.x, this.y, this.width, this.height);
+			
+			// Update x and y according to the direction to move and
+			// redraw the ship. Change the else if's to if statements
+			// to have diagonal movement.
+			if (KEY_STATUS.left) {
+				this.x -= this.speed
+				if (this.x <= 0) // Kep player within the screen
+					this.x = 0;
+			} else if (KEY_STATUS.right) {
+				this.x += this.speed
+				if (this.x >= this.canvasWidth - this.width)
+					this.x = this.canvasWidth - this.width;
+			} else if (KEY_STATUS.up) {
+				this.y -= this.speed
+				if (this.y <= this.canvasHeight/4*3)
+					this.y = this.canvasHeight/4*3;
+			} else if (KEY_STATUS.down) {
+				this.y += this.speed
+				if (this.y >= this.canvasHeight - this.height)
+					this.y = this.canvasHeight - this.height;
+			}
+			
+			// Finish by redrawing the ship
+			this.draw();
+		}
+		
+		if (KEY_STATUS.space && counter >= fireRate) {
+			this.fire();
+			counter = 0;
+		}
+	};
+	
+	/*
+	 * Fires two bullets
+	 */
+	this.fire = function() {
+		this.bulletPool.getTwo(this.x+6, this.y, 3,
+		                       this.x+33, this.y, 3);
+	};
+}
+Ship.prototype = new Drawable();
+
+
+ /**
+ * Creates the Game object which will hold all objects and data for
+ * the game.
+ */
+function Game() {
+	/*
+	 * Gets canvas information and context and sets up all game
+	 * objects. 
+	 * Returns true if the canvas is supported and false if it
+	 * is not. This is to stop the animation script from constantly
+	 * running on browsers that do not support the canvas.
+	 */
+	this.init = function() {
+		// Get the canvas elements
+		this.bgCanvas = document.getElementById('background');
+		this.shipCanvas = document.getElementById('ship');
+		this.mainCanvas = document.getElementById('main');
+		
+		// Test to see if canvas is supported. Only need to
+		// check one canvas
+		if (this.bgCanvas.getContext) {
+			this.bgContext = this.bgCanvas.getContext('2d');
+			this.shipContext = this.shipCanvas.getContext('2d');
+			this.mainContext = this.mainCanvas.getContext('2d');
+		
+			// Initialize objects to contain their context and canvas
+			// information
+			Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+			
+			Ship.prototype.context = this.shipContext;
+			Ship.prototype.canvasWidth = this.shipCanvas.width;
+			Ship.prototype.canvasHeight = this.shipCanvas.height;
+			
+			Bullet.prototype.context = this.mainContext;
+			Bullet.prototype.canvasWidth = this.mainCanvas.width;
+			Bullet.prototype.canvasHeight = this.mainCanvas.height;
+			
+			// Initialize the background object
+			this.background = new Background();
+			this.background.init(0,0); // Set draw point to 0,0
+			
+			// Initialize the ship object
+			this.ship = new Ship();
+			// Set the ship to start near the bottom middle of the canvas
+			var shipStartX = this.shipCanvas.width/2 - imageRepository.spaceship.width;
+			var shipStartY = this.shipCanvas.height/4*3 + imageRepository.spaceship.height*2;
+			this.ship.init(shipStartX, shipStartY,										 
+										 imageRepository.spaceship.width,
+			               imageRepository.spaceship.height);
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
+	// Start the animation loop
+	this.start = function() {
+		this.ship.draw();
+		animate();
+	};
+}
+
+
+/**
+ * The animation loop. Calls the requestAnimationFrame shim to
+ * optimize the game loop and draws all game objects. This
+ * function must be a gobal function and cannot be within an
+ * object.
+ */
+function animate() {
+	thisFrame = Date.now();
+	elapsed = thisFrame - lastFrame;
+	lastFrame = thisFrame;
+	var span = document.getElementById('fps').innerHTML =avgFramerate;
+	
+	requestAnimFrame( animate );
+	game.background.draw();
+	game.ship.move();
+	game.ship.bulletPool.animate(); 
+	
+	frameCount++;
+	elapsedCounter += elapsed;
+	if (elapsedCounter > 1000) {
+		elapsedCounter -= 1000;
+		avgFramerate = frameCount;
+		frameCount = 0;
+	}
+}
+
+
+// The keycodes that will be mapped when a user presses a button.
+// Original code by Doug McInnes
+KEY_CODES = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+}
+
+// Creates the array to hold the KEY_CODES and sets all their values
+// to true. Checking true/flase is the quickest way to check status
+// of a key press and which one was pressed when determining
+// when to move and which direction.
+KEY_STATUS = {};
+for (code in KEY_CODES) {
+  KEY_STATUS[KEY_CODES[code]] = false;
+}
+/**
+ * Sets up the document to listen to onkeydown events (fired when
+ * any key on the keyboard is pressed down). When a key is pressed,
+ * it sets the appropriate direction to true to let us know which
+ * key it was.
+ */
+document.onkeydown = function(e) {
+	// Firefox and opera use charCode instead of keyCode to
+	// return which key was pressed.
+	var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+  if (KEY_CODES[keyCode]) {
+		e.preventDefault();
+    KEY_STATUS[KEY_CODES[keyCode]] = true;
+  }
+}
+/**
+ * Sets up the document to listen to ownkeyup events (fired when
+ * any key on the keyboard is released). When a key is released,
+ * it sets teh appropriate direction to false to let us know which
+ * key it was.
+ */
+document.onkeyup = function(e) {
+  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+  if (KEY_CODES[keyCode]) {
+    e.preventDefault();
+    KEY_STATUS[KEY_CODES[keyCode]] = false;
+  }
+}
+
+
+/**	
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop, 
+ * otherwise defaults to setTimeout().
+ */
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
